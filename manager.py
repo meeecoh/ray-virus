5f2b48d6f73b444ba1b88191dc3a1bc1e1066f5c
meeecoh <125831029+Meeecoh@users.noreply.github.com>
2026-08-20 22:26:15 -0700

    move icon logic to class

--- manager.py
+++ manager.py
@@ -4,45 +4,53 @@ from pystray import Icon, Menu, MenuItem
 from time import sleep
 
 
-"""
-Window Manager for Ray Virus
-Opens ray viruses
-"""
-def on_action():
-    pass
+class RaySystemIcon:
+    """
+    Window Manager for Ray Virus
+    Opens ray viruses
+    """
+    def __init__(self):
+        self.virus_enabled = True
+        self.running = False
+        
+        virus_menu_toggle = MenuItem("Enable Virus", self.toggle_virus, checked=lambda x : self.virus_enabled)
+        menu = Menu(
+                virus_menu_toggle,
+                MenuItem("Exit", self.on_exit)
+            )
+        
+        self.icon = Icon(
+            name="RayVirusManager",
+            icon=Image.open("you_hacked.png").resize((64, 64)),
+            title="Ray Virus Manager",
+            menu=menu
+        )
 
-def on_exit():
-    """Stops tray application loop"""
-    icon.stop()
-    global program_running
-    program_running = False
-    
-    
-def toggle_virus():
-    global virus_enabled
-    virus_enabled =  not virus_enabled
+    def on_exit(self):
+        """Stops tray application loop"""
+        self.icon.stop()
+        self.running = False
+        
+        
+    def toggle_virus(self):
+        self.virus_enabled =  not self.virus_enabled
+        
+    def run_detached(self):
+        """Run loop handling events detached"""
+        self.running = True
+        self.icon.run_detached()
+        
+    def run(self):
+        """Run icon events"""
+        self.running = True
+        self.icon.run()
 
-program_running = True
-virus_enabled = True
-virus_menu_toggle = MenuItem("Enable Virus", toggle_virus, checked=lambda x : virus_enabled)
 
-# define menu
-menu = Menu(
-    virus_menu_toggle,
-    MenuItem("Exit", on_exit)
-)
-
-# define icon
-icon = Icon(
-    name="RayVirusManager",
-    icon=Image.open("you_hacked.png").resize((64, 64)),
-    title="Ray Virus Manager",
-    menu=menu
-)
 
 if __name__ == "__main__":
+    icon = RaySystemIcon()
     icon.run_detached()
     
-    while program_running:
-        sleep(1/30)
-        print(virus_enabled)
+    while icon.running:
+        sleep(1/12)
+        print(icon.virus_enabled)
